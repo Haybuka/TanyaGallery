e5fe4d8e06bef6a2a7e80420c6510dc2fc9f2a59
Haybuka <paschal.nduleo@gmail.com>
2020-07-13 16:24:38 +0100

    documentation for tanya

---
+++ assets/scripts/script.js
@@ -0,0 +1,23 @@
+let imgs = document.querySelectorAll('#imageGrid img');
+let newImg = document.querySelectorAll('img')
+let newImage = Array.from(imgs);
+console.log(newImage)
+const imgArray = [
+    'assets/images/Rectangle%2020-1.png',
+    'assets/images/Rectangle%2020-2.png',
+    'assets/images/Rectangle%2020-3.png',
+    'assets/images/Rectangle%2020-4.png',
+    'assets/images/Rectangle%2020-5.png',
+    'assets/images/Rectangle%2020-6.png',
+    'assets/images/Rectangle%2020-7.png',
+    'assets/images/Rectangle%2020-8.png',
+    'assets/images/Rectangle%20.png'
+];
+let timed = setInterval(() => {
+    for (let i = 0; i <= newImage.length; i++) {
+        if (i === 8) {
+            break;
+        }
+        newImage[i].src = imgArray[Math.floor(Math.random() * imgArray.length)];
+    }
+}, 5000);
